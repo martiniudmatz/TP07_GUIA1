06572369756f1fb02630262f123a445613f33504
martiniudmatz <martiniudmatz@gmail.com>
2026-04-17 09:01:59 -0300

    Initial commit

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+
+    string nombre = "martin";
+    int edad = 16;
+    float estatura = 1.79f;
+    bool esDonante = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        string mensajeDonante;
+
+        if (esDonante){
+            mensajeDonante = "y soy donante";
+        }
+
+        else{
+            mensajeDonante = "y no soy donante";
+        }
+        Debug.Log(" Mi nombre es " +  nombre + ",  tengo " + edad + ", y mido " + estatura + mensajeDonante);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DeclaracionDeVariable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariable : MonoBehaviour
+{
+
+    int variableUno = 2;
+    float variableDos = 8;
+    string variableTres = "Hola, variable nueva";
+    bool variableCuatro = true;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        Debug.Log(variableUno);
+        Debug.Log(variableDos);
+        Debug.Log(variableTres);
+        Debug.Log(variableCuatro);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
